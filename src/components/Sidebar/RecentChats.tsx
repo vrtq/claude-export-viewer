945d090b2105d46bb423d1fc3cdb9aca0e1ca65b
6voo <97762571+vrtq@users.noreply.github.com>
2026-07-10 00:56:47 -0400

    feat: Load conversations on recent chats list

--- src/components/Sidebar/RecentChats.tsx
+++ src/components/Sidebar/RecentChats.tsx
@@ -1,22 +1,38 @@
+import { type Conversation } from "@/types";
 import { get } from "idb-keyval";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function RecentChats() {
-    const [recentChats, setRecentChats] = useState([]);
+    const [recentChats, setRecentChats] = useState<Conversation[]>([]);
 
     useEffect(() => {
         (async () => {
-            //setRecentChats(await get('conversations'))
+            const conversations: Conversation[] = await get('conversations') ?? [];
+            setRecentChats(conversations)
         })();
-
     }, []);
 
     return (
-        <div className="text-muted-foreground font-sans">
-            <p className="text-muted-foreground/75 text-sm p-2 pl-0">Recents</p>
-            <ul className="pt-2 text-foreground-secondary">
-                <li>Nothing yet...</li>
+        <div className="text-muted-foreground font-sans h-full overflow-auto">
+            <p className="text-muted-foreground/75 text-xs p-2 pl-0">Recents</p>
+            <ul className="flex flex-col -mt-2">
+                {
+                    recentChats.length > 0
+                    ? recentChats.map(chat => <Chat data={chat} /> )
+                    : <li key="nothing" className="text-foreground-secondary">Nothing yet...</li>
+                }
             </ul>
         </div>
+    )
+}
+
+function Chat({ data }: { data: Conversation; }) {
+    return (
+        <li key={data.uuid} className="mt-1 -translate-x-2 p-2 text-foreground-secondary rounded-lg hover:bg-accent hover:text-foreground">
+            <Link to={`/chat/${data.uuid}`}>
+                <p className="text-sm -2 overflow-hidden text-ellipsis truncate ">{data.name}</p>
+            </Link>
+        </li>
     )
 }
