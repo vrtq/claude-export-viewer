a3dedc6f025955fe81e3528af0b3a99bbb3c0fe4
6voo <97762571+vrtq@users.noreply.github.com>
2026-07-10 17:26:35 -0400

    feat: Added search logic

--- src/pages/Recents.tsx
+++ src/pages/Recents.tsx
@@ -4,17 +4,19 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Recents() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     return (
         <div className="p-8 w-full bg-background h-full flex flex-col items-center ">
             <div className="flex flex-col h-full w-3/4 lg:w-2/3">
-                <Header />
-                <ChatList />
+                <Header setSearchQuery={setSearchQuery} />
+                <ChatList searchQuery={searchQuery} />
             </div>
         </div>
     )
 }
 
-function Header() {
+function Header({ setSearchQuery }: { setSearchQuery: React.Dispatch<React.SetStateAction<string>> }) {
     return (
         <div className="w-full flex flex-col font-sans gap-4 bg-background p-2">
             <div className="flex flex-row justify-between">
@@ -23,12 +25,24 @@ function Header() {
                     <span className="text-foreground-secondary">Filter by ...</span>
                 </button>
             </div>
-            <SearchBox />
+            <SearchBox setSearchQuery={setSearchQuery} />
         </div>
     )
 }
 
-function SearchBox() {
+function SearchBox({ setSearchQuery }: { setSearchQuery: React.Dispatch<React.SetStateAction<string>> }) {
+    const [searchInput, setSearchInput] = useState("");
+
+    useEffect(() => {
+        const debounce = setTimeout(() => {
+            setSearchQuery(searchInput);
+        }, 400);
+
+        return () => {
+            clearTimeout(debounce);
+        }
+    }, [searchInput, setSearchQuery]);
+
     return (
         <div className="w-full h-12 bg-card rounded-xl p-2 px-4 gap-4  flex flex-row text-muted-foreground items-center focus-within:outline-1 focus-within:outline-[#2a78d6] focus-within:border focus-within:border-black focus-within:shadow-[0px_0px_4px_#2a78d6] ">
             <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} viewBox="0 0 24 24">
@@ -37,22 +51,47 @@ function SearchBox() {
                     <circle cx={11} cy={11} r={8}></circle>
                 </g>
             </svg>
-            <input type="text" className="text-foreground text-sm" placeholder="Search Chats..." />
+            <input value={searchInput} type="text" className="text-foreground text-sm w-full" placeholder="Search Chats..." 
+                onChange={(e) => {
+                    setSearchInput(e.target.value);
+                }}
+            />
         </div>
     )
 }
 
-function ChatList() {
+function ChatList({ searchQuery }: { searchQuery: string }) {
     const [recentChats, setRecentChats] = useState<Conversation[]>([]);
+    const [allChats, setAllChats] = useState<Conversation[]>([]);
 
+    // Fetch all chats on mount and sort by date (first to last)
     useEffect(() => {
         (async () => {
+            const sortByDate = (array: Conversation[]): Conversation[] => 
+                array.sort((a, b) =>
+                    new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
+                )
+
             const conversations: Conversation[] = await get('conversations') ?? [];
-            setRecentChats(conversations.sort((a, b) =>
-                new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
-                ))
+            
+            setRecentChats(sortByDate(conversations));
+            setAllChats(sortByDate(conversations));
         })();
     }, []);
+
+    useEffect(() => {
+        if (!searchQuery || searchQuery === "") {
+            setRecentChats(allChats);
+            return;
+        };
+
+        const matchedChats = allChats.filter(chat => 
+            chat.name.includes(searchQuery)
+            || chat.chat_messages.some(message => message.text.includes(searchQuery))
+        );
+        
+        setRecentChats(matchedChats);
+    }, [searchQuery]);
 
     return (
         <div className="h-full w-full overflow-y-auto">
@@ -69,7 +108,7 @@ function ChatList() {
 
 function Chat({ data }: { data: Conversation; }) {
     return (
-        <li className="peer border-t-card border-t first:border-t-0 hover:border-0 [.peer:hover+&]:border-0 ">
+        <li key={data.uuid} className="peer border-t-card border-t first:border-t-0 hover:border-0 [.peer:hover+&]:border-0">
             <Link to={`/chat/${data.uuid}`} className="flex flex-row p-2 justify-between rounded-md hover:bg-card">
                 <p className="">{data.name}</p>
                 <p className="text-muted-foreground text-sm">{new Date(data.updated_at).toLocaleDateString()}</p>
