543673692a704cdaa0bdb61b65f27c4ca58afb57
6voo <97762571+vrtq@users.noreply.github.com>
2026-07-10 14:54:51 -0400

    style: Sort recent chats by last updated date

--- src/pages/Recents.tsx
+++ src/pages/Recents.tsx
@@ -48,7 +48,9 @@ function ChatList() {
     useEffect(() => {
         (async () => {
             const conversations: Conversation[] = await get('conversations') ?? [];
-            setRecentChats(conversations)
+            setRecentChats(conversations.sort((a, b) =>
+                new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
+                ))
         })();
     }, []);
 
